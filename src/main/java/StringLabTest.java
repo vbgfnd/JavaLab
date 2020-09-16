e3d9ad356db8aa63cc4928190365e0955fb55b42
master <liyuanming@coding.net>
2020-09-16 14:25:15 +0800

    add javac complie option=utf-8,add StringLab

---
+++ src/main/java/StringLabTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class StringLabTest {
+    //测试删除字串内指定字符
+    @Test
+    public void StringReplace(){
+        String str = "22.33.44.55";
+        String str2 = str.replace(".","");
+        System.out.println(str2);
+    }
+}
